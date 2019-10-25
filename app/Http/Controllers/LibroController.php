e4a99a278118606bffb7af91f51cc98a2caa3b69
Sergio <djsergioxl@hotmail.com>
2019-10-25 19:44:50 +0200

    Subida
    Proyecto Biblioteca subdio

---
+++ app/Http/Controllers/LibroController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Libro;
+
+class LibroController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //dd(session()->all());
+        can('listar-libros');
+        $datas = Libro::orderBy('id')->get();
+        return view('libro.index', compact('datas'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function crear()
+    {
+        can('crear-libros');
+        return view('libro.crear');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function guardar(Request $request)
+    {
+        if ($foto = Libro::setCaratula($request->foto_up))
+            $request->request->add(['foto' => $foto]);
+        Libro::create($request->all());
+        return redirect()->route('libro')->with('mensaje', 'El libro se creo correctamente');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function ver(Libro $libro)
+    {
+        return view('libro.ver', compact('libro'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function editar($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function actualizar(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function eliminar($id)
+    {
+        //
+    }
+}
